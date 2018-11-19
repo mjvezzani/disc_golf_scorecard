e34730e6f2df030d262286bb484d2f73de49ceba
Michael Vezzani <mvezzani@callibrity.com>
2018-11-18 20:24:26 -0700

    Add database seed data and routes
    The Courses and Holes tables now have seeder data. This seeder data is
    in the format of an exported javascript object. Adding more seeder data
    to this file is a simple matter of adding the appropriate rows.
    
    The courses seeder data is in the format of:
    
    [COURSE_NAME,NUMBER_OF_HOLES,COURSE_PAR,ADDRESS]
    
    The holes seeder data is in the format of:
    
    [HOLE_NUMBER,COURSE_FOREIGN_KEY,PAR]
    
    At the present moment, there are 3 routes:
    - `/api/courses`
    - `/api/courses/:id`
    - `/api/courses/:id/holes`.
    
    The first route returns a JSON object that contains all data from all
    courses. The second route returns data for a single course. The third
    route returns all data about a given course's holes.

--- database/server.js
+++ database/server.js
@@ -1,3 +1,5 @@
+const {courses, holes} = require('./seed');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
@@ -12,11 +14,19 @@ app.use(cors());
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');
 db.serialize(function () {
-  db.run("CREATE TABLE rounds (id INTEGER PRIMARY KEY, course_id INTEGER, total_score INTEGER)");
+  // Setup Courses Table
   db.run("CREATE TABLE courses (id INTEGER PRIMARY KEY, name TEXT, holes INTEGER, par INTEGER, address TEXT)");
+  courses.forEach((course) => db.run("INSERT INTO courses (name, holes, par, address) VALUES (?, ?, ?, ?)", course));
+
+  // Setup Holes Table
   db.run("CREATE TABLE holes (id INTEGER PRIMARY KEY, number INTEGER, course_id INTEGER, par INTEGER)");
-  db.run("CREATE TABLE scores (id INTEGER PRIMARY KEY, filename TEXT, location TEXT, owner INTEGER)");
-  db.run("INSERT INTO courses (name, holes, par, address) VALUES (?, ?, ?, ?)", "Roots Disc Golf Course", 18, 54, "Some place on redwood road");
+  holes.forEach((hole) => db.run("INSERT INTO holes (number, course_id, par) VALUES (?, ?, ?)", hole));
+
+  // Setup Rounds Table
+  db.run("CREATE TABLE rounds (id INTEGER PRIMARY KEY, course_id INTEGER, total_score INTEGER)");
+
+  // Setup Scores Table
+  db.run("CREATE TABLE scores (id INTEGER PRIMARY KEY, points INTEGER, round_id INTEGER, hole_id INTEGER)");
 });
 
 var port = 4000;
@@ -31,6 +41,12 @@ router.get('/courses', function(req, res) {
 router.get('/courses/:id', function(req, res) {
   db.get("SELECT * FROM courses WHERE id = (?)", [req.params.id], function(err, row){
     res.json({ course: row});
+  });
+});
+
+router.get('/courses/:id/holes', function(req, res) {
+  db.all("SELECT * FROM holes WHERE course_id = (?)", [req.params.id], function(err, rows){
+    res.json({ course: { holes: rows }});
   });
 });
 
